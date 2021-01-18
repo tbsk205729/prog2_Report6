d7c4d74e6edfab32fbbeabe00c870cd8c3958288
Nanako NakayoshiA <e205729@ie.u-ryukyu.ac.jp>
2021-01-19 02:18:16 +0900

    3rd commit

--- src/jp/ac/uryukyu/ie/e205729/Command.java
+++ src/jp/ac/uryukyu/ie/e205729/Command.java
@@ -1,7 +1,18 @@
 package jp.ac.uryukyu.ie.e205729;
 import java.util.Scanner;
 
-public class Command {
+public class Command extends Data {
+
+    public void Command_input(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("数値を入力して選択 \n 1:科目の単位数を記録 \n 2:現在の総単位数を表示 \n");
+        
     
+        
+        scanner.close();
+
+    }
     
+
 }
