acc2e592152114c3c90504dfede68d55a75de825
Nanako NakayoshiA <e205729@ie.u-ryukyu.ac.jp>
2021-01-19 02:20:11 +0900

    3rd commit

--- src/jp/ac/uryukyu/ie/e205729/Main.java
+++ src/jp/ac/uryukyu/ie/e205729/Main.java
@@ -5,9 +5,11 @@ public class Main {
     public static void main(String[] args){
 
         var data = new Data();
+        var command = new Command();
 
         data.PrintTest();
-    
+
+        command.Command_input();
 
     }
     
